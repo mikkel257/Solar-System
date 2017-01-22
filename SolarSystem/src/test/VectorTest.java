a0186cc44b5d265da4ad7a0878da19e1a92920ed
Mikkel <Mikkel@DESKTOP-V5GTBFL>
2017-01-22 13:58:11 +0100

    Added entity package

---
+++ SolarSystem/src/test/VectorTest.java
@@ -0,0 +1,60 @@
+package test;
+package default;
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class VectorTest {
+
+	Vector thisVector = new Vector(1,2,3);
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testVectorDoubleArray() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testVectorDoubleDoubleDouble() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetElement() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetEuclideanNorm() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testLength() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAdd() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSubtract() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEqualsVector() {
+		fail("Not yet implemented");
+	}
+
+}
