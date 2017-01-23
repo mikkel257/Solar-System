4d5b5ea345503e8e7025c1d36b5a1243061df5bc
Mikkel <Mikkel@DESKTOP-V5GTBFL>
2017-01-23 12:17:26 +0100

    Sun
    - Added class Sun extends from HeavenlyBody
    - Added constructor.

---
+++ SolarSystem/src/entity/Sun.java
@@ -0,0 +1,16 @@
+package entity;
+
+import java.awt.Color;
+
+public class Sun extends HeavenlyBody {
+
+	public Sun(String name, Vector position, Vector velocity, Color color)
+	{
+		super(name, position, velocity, color);
+	}
+	
+	public void updateInformation(double timeStep)
+	{
+		
+	}
+}
