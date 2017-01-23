c7eaff94be6024c2a224429e36dc4d5ae9da1248
Mikkel <Mikkel@DESKTOP-V5GTBFL>
2017-01-23 12:02:49 +0100

    Heavenly body
    - Added a class HeavenlyBody
    - Added a constructor to the class.

---
+++ SolarSystem/src/entity/HeavenlyBody.java
@@ -0,0 +1,21 @@
+package entity;
+
+import java.awt.Color;
+
+public abstract class HeavenlyBody {
+	
+	//Instance variables
+	protected String name;
+	protected Vector position;
+	protected Vector velocity;
+	protected Color color;
+	
+	public HeavenlyBody(String name, Vector position, Vector velocity, Color color)
+	{
+		this.name = name;
+		this.position = position;
+		this.velocity = velocity;
+		this.color = color;
+	}
+
+}
